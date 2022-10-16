27bad340eac000b817a1b9b19d76ba2f8606f559
Theodor Dumitru <teddy.dumitru@yahoo.com>
2022-10-16 18:27:13 -0400

    With Comments

--- Team4-Code.py
+++ Team4-Code.py
@@ -1,11 +1,13 @@
 import xlrd
 
+#creates a dictionary from the excel table
 def getDict():
     workbook = xlrd.open_workbook("Team4-Table.xls")
     sheet = workbook.sheet_by_index(0)
 
     dict = {}
 
+    #iterates through the excel
     for i in range(73):
         char = sheet.cell(i,0).value
         binary = sheet.cell(i,1).value
@@ -16,8 +18,10 @@ def getDict():
 
 charMap = getDict()
 
+#gets the file name the user is trying to encode
 file_name = input("enter the name of the file to encode: ")
 try:
+    #gets the file and creates a new file named "BinOutput.txt"
     code_file = open(file_name)
     my_str = code_file.read()
     code_output = open("BinOutput.txt", "w+")
@@ -25,13 +29,18 @@ try:
     result = ''
     bits = 0
 
+    #while my_str still has characters, it iterates through and codes
+    #each character. It gets the first character in string, converts
+    #it using the dictionary and then adds it to the final string.
     while len(my_str) > 0:
         add = charMap[my_str[0]]
         result+=add
         bits+=len(add)
         my_str = my_str[1:]
-
+    
+    #writes to the the file "BinOutput.txt"
     code_output.write(str(bits) + "." + result)
 except:
+    #if it errors, it tells the user that they inputed the wrong file
     print("No file named that.")
 
--- Team4-Decode.py
+++ Team4-Decode.py
@@ -1,11 +1,14 @@
 import xlrd
 
+# function of dictionary
 def dic():
+    # open the Excel file and get the first sheet
     wb = xlrd.open_workbook("Team4-Table.xls")
     sh = wb.sheet_by_index(0)
 
     Bin2Char = {}
 
+    # read all the characters and put them to the dictionary
     for i in range(0,73):
         char = sh.cell(i, 0).value
         binary = sh.cell(i, 1).value
@@ -14,26 +17,30 @@ def dic():
 
     return Bin2Char
 
+# assign the dictionary 
 Bin2Char = dic()
 file_name = input("enter the name of the file to decode: ")
-
+# get the file name from user and read from it
 file = open(file_name,"r")
 binary = file.read()
 result = ""
 binary = str(binary.split(".")[1])
 
 #5 short "1"      7 long "0"
+# while loop run as long as there is still number in it
 while binary!="":
+    # determine if this part is short base on the beginning
     if binary[0] == "1":
         temp = Bin2Char[binary[0:5]]
         binary = binary[5:]
-
+    # determine if this part is long base on the beginning
     elif binary[0] == "0":
         temp = Bin2Char[binary[0:7]]
         binary = binary[7:]
 
     result += temp
 
+# open the output file and output the results
 file_decode = open("TextOutput.txt","w")
 file_decode.write(result)
 
